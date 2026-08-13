83231eb8d826a4a019d220654a5f9eb523d161eb
Barbara <barbara.oktoi@student.moringaschool.com>
2026-08-13 22:32:04 +0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,18 @@ import Home from './pages/Home'
 const Products =() => <div className="p-10 text-2xl font-bold text-center">Products Page</div>
 const Orders =() => <div className="p-10 text-2xl font-bold text-center">Orders Page</div>
 const Login =() => <div className="p-10 text-2xl font-bold text-center">Login Page</div>
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" elements={<Products />} />
+        <Route path="/orders" elements={<Orders />} />
+        <Route path="/login" elements={<Login />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
