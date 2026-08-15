28ae1b592c1151f2dee20f59597845e4977403a5
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-08-16 02:09:09 +0300

    input field for email

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,8 +35,13 @@ const Register = () => {
         <h2 className="text-2xl font-bold text-center mb-4">Create Account</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div></div>
-{/*  */}
+{/*  creates names input field */}
                 <label className="block text-gray-700 mb-1">Name</label>
+                {/* input field for name connecting to the state */}
             <input value={name} onChange={(e) => setName(e.target.value)} className="w-full px-4 py-2 border rounded" />
+          </div>
+          <div></div>
+                     <label className="block text-gray-700 mb-1">Email</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-4 py-2 border rounded" />
           </div>
           <div></div>
