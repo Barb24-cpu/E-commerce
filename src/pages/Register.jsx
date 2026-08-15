5243d2893710d8513627db9f89a6fe57af33f724
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-08-16 01:46:22 +0300

    call backend api takes process by async await and saves the detais

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,3 +8,16 @@ const Register = () => {
 //   create state for password and error
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
+
+//   calling backend API to register the user
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      const res = await fetch('http://localhost:5176/api/register', 
+        {
+            // save the name, email, and password in the body of the request
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, email, password }),
+      });
