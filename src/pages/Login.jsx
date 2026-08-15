9c4cf8bb6920ebaa8f3db966b4d7e097e66b40c9
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-08-16 01:25:01 +0300

    show button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -123,8 +123,9 @@ const BuyerLoginPage = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full px-4 py-2 pr-16 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FF6A00]"
               />
-
+{/* Show/Hide Password Button */}
               <button
+            
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
                 className="absolute right-3 top-2 text-gray-500 hover:text-gray-700"
@@ -133,7 +134,7 @@ const BuyerLoginPage = () => {
                 {showPassword ? "Hide" : "Show"}
               </button>
             </div>
-
+{/*  */}
             {errors.password && (
               <p className="text-red-500 text-sm mt-1">
                 {errors.password}
