793f70972ee9feb8fce44f3988ba911410ee12a7
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-08-16 14:25:43 +0300

    create a dir&user.json file dont Exist

--- server.js
+++ server.js
@@ -15,3 +15,8 @@ app.use(express.json());
 // define the path to the users.json file
 const dataDir = path.resolve('./data');
 const usersFile = path.join(dataDir, 'users.json');
+
+
+// create the data directory and users.json file if they don't exist
+if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir);
+if (!fs.existsSync(usersFile)) fs.writeFileSync(usersFile, JSON.stringify([]));
