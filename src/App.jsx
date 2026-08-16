3e1b6c83de79e270d38f9ddd68fbe1e530dd278d
Barbara <barbara.oktoi@student.moringaschool.com>
2026-08-16 20:12:33 +0300

    Fixed bug in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,11 @@ import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Orders from "./pages/Orders";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-// Placeholder views page
-const Products = () => <div className="p-10 text-2xl font-bold text-center">Products Page</div>;
-const Orders = () => <div className="p-10 text-2xl font-bold text-center">Orders Page</div>;
+
 
 function App() {
   return (
@@ -33,6 +30,11 @@ function App() {
       <Route path="/checkout" element={<Checkout />} />
 
       <Route path="/orders" element={<Orders />} />
+
+      <Route path="/Login" element={<Login />} />
+
+      <Route path="/Register" element={<Register />} />
+
     </Routes>
   );
 }
